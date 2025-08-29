5155fd9f3649921ae87e530925db92badca40aa4
NachoOFC <ignacioperezolavarria@gmail.com>
2025-08-29 14:50:04 -0400

    fix redirect

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Page() {
   // Redirige la raíz a la página de login para que sea lo primero que vea el usuario
   redirect('/login');
